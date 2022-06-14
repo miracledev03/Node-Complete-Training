5a90bdf5a4c7252281164806aa56f3e54b3a560c
unknown <miracledev0303@gmail.com>
2022-06-14 14:57:57 -0300

    create and load a module

--- first-app/app.js
+++ first-app/app.js
@@ -1,10 +1,19 @@
 function sayHello(name) {
-  console.log('Hello ' + name);
+  console.log("Hello " + name);
 }
 
-sayHello('Mirade');
+// sayHello("Mirade");
 
 // console.log(window);
 
-console.log(module);
+// console.log(module);
 
+// const logger = require("./logger");
+
+// console.log(logger);
+
+// logger.log("message");
+
+const log = require("./logger");
+
+log("message");
