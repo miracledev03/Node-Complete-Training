ffc7057986f548d234a27ec13d75756b0058c202
unknown <miracledev0303@gmail.com>
2022-06-15 15:23:58 -0300

    Middleware

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,8 +1,17 @@
+const morgan = require('morgan');
+const helmet = require('helmet');
 const Joi = require("joi");
+const logger = require("./logger");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(helmet());
+app.use(morgan('tiny'));
+
+app.use(logger);
 
 const courses = [
   {
