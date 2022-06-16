47e2a404942ccdb163f2bb1ba8cbdfc51eaeacc6
unknown <miracledev0303@gmail.com>
2022-06-16 15:01:09 -0300

    Parallel Promises

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,7 +1,31 @@
 // Creating settled Promises
 
-const resolve = Promise.resolve({ id: 1});
-resolve.then(result => console.log(result));
+// const resolve = Promise.resolve({ id: 1 });
+// resolve.then((result) => console.log(result));
 
-const reject = Promise.reject(new Error('reason for rejection...'));
-reject.catch(error => console.log(error));
+// const reject = Promise.reject(new Error("reason for rejection..."));
+// reject.catch((error) => console.log(error));
+
+// Parallel Promises
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Async operation 1...");
+    resolve(1);
+    // reject(new Error("Failed for some reason"));
+  }, 2000);
+});
+
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async operation 2...");
+    resolve(2);
+  }, 2000);
+});
+
+Promise.all([p1, p2])
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error.message));
+
+Promise.race([p1, p2])
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error.message));
