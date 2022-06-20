626ae1a49a42244822ddb254eb362e791a03864b
unknown <miracledev0303@gmail.com>
2022-06-20 18:26:47 -0300

    solution 2

---
+++ mongo-exercise/solution2.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/mongo-exercises");
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: Date,
+  isPublished: Boolean,
+  price: Number,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+const getCourses = async () => {
+  return await Course.find({
+    isPublished: true,
+    tags: { $in: ["frontend", "backend"] },
+  })
+    .or([{ tags: "frontend" }, { tags: "backend" }])  // Same as above tags filter
+    .sort("-price")
+    .select("name author price");
+};
+
+const run = async () => {
+  const courses = await getCourses();
+
+  console.log(courses);
+};
+
+run();
