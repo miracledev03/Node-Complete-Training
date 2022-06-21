123b254b28b79396ce2960b09475a557d1ef9550
unknown <miracledev0303@gmail.com>
2022-06-21 16:35:36 -0300

    built movies api

--- vidly/models/genre.js
+++ vidly/models/genre.js
@@ -1,17 +1,16 @@
 const Joi = require("joi");
 const mongoose = require("mongoose");
 
-const Genre = mongoose.model(
-  "Genre",
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-  })
-);
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+});
+
+const Genre = mongoose.model("Genre", genreSchema);
 
 function validateGenre(genre) {
   const schema = {
@@ -23,3 +22,4 @@ function validateGenre(genre) {
 
 exports.Genre = Genre;
 exports.validateGenre = validateGenre;
+exports.genreSchema = genreSchema;
