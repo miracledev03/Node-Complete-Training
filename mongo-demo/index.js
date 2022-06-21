e64bb8184e87672ab4e461a30801367e44b7b4c2
unknown <miracledev0303@gmail.com>
2022-06-21 08:46:55 -0300

    async validators:'

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -24,11 +24,16 @@ const courseSchema = new mongoose.Schema({
   tags: {
     type: Array,
     validate: {
-      validator: function(v) {
-        return v && v.length > 0;
+      isAsync: true,
+      validator: function (v, callback) {
+        setTimeout(() => {
+          // Do some async work
+          const result = v && v.length > 0;
+          callback(result);
+        }, 4000);
       },
-      message: 'A course should have at least one tag.'
-    }
+      message: "A course should have at least one tag.",
+    },
   },
   date: {
     type: Date,
