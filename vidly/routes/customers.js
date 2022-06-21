6cfbb1e3f61749fd87e062416263462920876e88
unknown <miracledev0303@gmail.com>
2022-06-21 10:40:21 -0300

    restructure project by splitting model and api

--- vidly/routes/customers.js
+++ vidly/routes/customers.js
@@ -1,26 +1,6 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const { Customer, validateCustomer } = require('../models/customer');
 const express = require('express');
 const router = express.Router();
-
-const Customer = mongoose.model('Customer', new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  },
-  isGold: {
-    type: Boolean,
-    default: false
-  },
-  phone: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  }
-}));
 
 router.get('/', async (req, res) => {
   const customers = await Customer.find().sort('name');
@@ -72,15 +52,5 @@ router.get('/:id', async (req, res) => {
 
   res.send(customer);
 });
-
-function validateCustomer(customer) {
-  const schema = {
-    name: Joi.string().min(5).max(50).required(),
-    phone: Joi.string().min(5).max(50).required(),
-    isGold: Joi.boolean()
-  };
-
-  return Joi.validate(customer, schema);
-}
 
 module.exports = router; 
--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -1,19 +1,6 @@
-const Joi = require("joi");
-const mongoose = require("mongoose");
+const { Genre, validateGenre } = require('../models/genre');
 const express = require("express");
 const router = express.Router();
-
-const Genre = mongoose.model(
-  "Genre",
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-  })
-);
 
 router.get("/", async (req, res) => {
   const genres = await Genre.find().sort('name');
@@ -61,13 +48,5 @@ router.get("/:id", async (req, res) => {
 
   res.send(genre);
 });
-
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-
-  return Joi.validate(genre, schema);
-}
 
 module.exports = router;
