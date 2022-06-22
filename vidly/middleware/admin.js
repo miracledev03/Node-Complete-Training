b5e6be77617c2beb9bf20105c3ff787aa9eacc76
unknown <miracledev0303@gmail.com>
2022-06-22 18:58:56 -0300

    role based authorization

--- vidly/middleware/admin.js
+++ vidly/middleware/admin.js
@@ -2,6 +2,8 @@ module.exports = function (req, res, next) {
   // 401 Unauthorized
   // 403 Forbidden
 
+  console.log(req.user);
+
   if (!req.user.isAdmin) return res.status(403).send('Access denied.');
 
   next();
