10f17e27d10fa9a29b08def1f89937b38c8b1259
unknown <miracledev0303@gmail.com>
2022-06-22 15:38:44 -0300

    Add headers to response headers

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -1,5 +1,7 @@
-const bcrypt = require('bcrypt');
-const _ = require ('lodash');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
 const { User, validateUser } = require("../models/user");
 const express = require("express");
 const router = express.Router();
@@ -11,14 +13,17 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered.");
 
-  user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
 
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
 
-  res.send(_.pick(user, ['_id', 'name', 'email']));
+  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
