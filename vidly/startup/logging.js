e4a731bc22551baf03de968698d4f1330be1c168
unknown <miracledev0303@gmail.com>
2022-06-23 14:24:56 -0300

    showing unhandled exceptions on the console

--- vidly/startup/logging.js
+++ vidly/startup/logging.js
@@ -2,8 +2,9 @@ require("express-async-errors");
 const winston = require("winston");
 require("winston-mongodb");
 
-module.exports = function() {
+module.exports = function () {
   winston.handleExceptions(
+    new winston.transports.Console({ colorize: true, prettyPrint: true }),
     new winston.transports.File({ filename: "uncaughtExceptions.log" })
   );
 
@@ -18,4 +19,4 @@ module.exports = function() {
       level: "error",
     })
   );
-}
+};
