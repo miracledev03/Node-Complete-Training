f08c5c70d1f3d6a081df66c6bbfd29bb4cd192cb
unknown <miracledev0303@gmail.com>
2022-06-23 12:14:01 -0300

    unhandled promise rejections

--- vidly/index.js
+++ vidly/index.js
@@ -15,10 +15,23 @@ const auth = require("./routes/auth");
 const express = require("express");
 const app = express();
 
-process.on('uncaughtException', (ex) => {
-  console.log('WE GOT AN UNCAUGHT EXCEPTION');
-  winston.error('ex.message', ex);
-})
+// process.on("uncaughtException", (ex) => {
+//   console.log("WE GOT AN UNCAUGHT EXCEPTION");
+//   winston.error("ex.message", ex);
+//   process.exit(1);
+// });
+
+winston.handleExceptions(
+  new winston.transports.File({ filename: "uncaughtExceptions.log" })
+);
+
+process.on("unhandledRejection", (ex) => {
+  // console.log("WE GOT AN UNHANDLED REJECTION");
+  // winston.error("ex.message", ex);
+  // process.exit(1);
+
+  throw ex;
+});
 
 winston.add(new winston.transports.File({ filename: "logfile.log" }));
 winston.add(
@@ -28,7 +41,9 @@ winston.add(
   })
 );
 
-throw new Error('Something failed during startup.');
+const p = Promise.reject(new Error("Something failed miserably!"));
+
+p.then(() => console.log("Done"));
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined");
