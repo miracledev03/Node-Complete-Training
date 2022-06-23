cb2aeaa347df4963c5958c23fdcf7452d19ec44f
unknown <miracledev0303@gmail.com>
2022-06-23 14:16:41 -0300

    extract logging

--- vidly/index.js
+++ vidly/index.js
@@ -1,52 +1,17 @@
-require("express-async-errors");
-const winston = require("winston");
-require("winston-mongodb");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const app = express();
 
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
-
-// process.on("uncaughtException", (ex) => {
-//   console.log("WE GOT AN UNCAUGHT EXCEPTION");
-//   winston.error("ex.message", ex);
-//   process.exit(1);
-// });
-
-winston.handleExceptions(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-
-process.on("unhandledRejection", (ex) => {
-  // console.log("WE GOT AN UNHANDLED REJECTION");
-  // winston.error("ex.message", ex);
-  // process.exit(1);
-
-  throw ex;
-});
-
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-winston.add(
-  new winston.transports.MongoDB({
-    db: "mongodb://localhost/vidly",
-    level: "error",
-  })
-);
-
-const p = Promise.reject(new Error("Something failed miserably!"));
-
-p.then(() => console.log("Done"));
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined");
   process.exit(1);
 }
-
-// Express Error Middleware
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
