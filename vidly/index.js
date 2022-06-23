61d0e69ca3fa5a2ce6675a662ac291362cf53622
unknown <miracledev0303@gmail.com>
2022-06-23 14:05:26 -0300

    extracting routes

--- vidly/index.js
+++ vidly/index.js
@@ -1,19 +1,14 @@
 require("express-async-errors");
 const winston = require("winston");
 require("winston-mongodb");
-const error = require("./middleware/error");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
 const express = require("express");
 const app = express();
+
+require('./startup/routes')(app);
 
 // process.on("uncaughtException", (ex) => {
 //   console.log("WE GOT AN UNCAUGHT EXCEPTION");
@@ -54,14 +49,6 @@ mongoose
   .connect("mongodb://localhost/vidly")
   .then(() => console.log("Connected to MongoDB..."))
   .catch(() => console.error("Could not connect to MongoDB..."));
-
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
 
 // Express Error Middleware
 app.use(error);
