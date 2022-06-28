038581916c002aea8a26453ce2b391fab5e45d7a
unknown <miracledev0303@gmail.com>
2022-06-28 18:14:53 -0300

    populating the database

---
+++ vidly/tests/integration/returns.test.js
@@ -0,0 +1,39 @@
+const request = require("supertest");
+const { Rental } = require("../../models/rental");
+const mongoose = require("mongoose");
+
+describe("/api/returns", () => {
+  let server;
+  let customerId = mongoose.Types.ObjectId();
+  let movieId = mongoose.Types.ObjectId();
+  let rental;
+
+  beforeEach(async () => {
+    server = require("../../index");
+
+    rental = new Rental({
+      customer: {
+        _id: customerId,
+        name: "12345",
+        phone: "12345",
+      },
+      movie: {
+        _id: movieId,
+        title: "12345",
+        dailyRentalRate: 2,
+      },
+    });
+
+    await rental.save();
+  });
+
+  afterEach(async () => {
+    server.close();
+    await Rental.remove();
+  });
+
+  it("should work!", async () => {
+    const result = await Rental.findById(rental._id);
+    expect(result).not.toBeNull();
+  });
+});
