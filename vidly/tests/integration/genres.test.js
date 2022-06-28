0e89f1dab085a26933cf1a66ad8b2a9c012ae8b2
unknown <miracledev0303@gmail.com>
2022-06-28 15:44:04 -0300

    testing routes with parameters

--- vidly/tests/integration/genres.test.js
+++ vidly/tests/integration/genres.test.js
@@ -23,8 +23,20 @@ describe("/api/genres", () => {
       const res = await request(server).get("/api/genres");
       expect(res.status).toBe(200);
       expect(res.body.length).toBe(2);
-      expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
-      expect(res.body.some(g => g.name === 'genre2')).toBeTruthy();
+      expect(res.body.some((g) => g.name === "genre1")).toBeTruthy();
+      expect(res.body.some((g) => g.name === "genre2")).toBeTruthy();
+    });
+  });
+
+  describe("GET /:id", () => {
+    it("should return a genre if valid id is passed", async () => {
+      const genre = new Genre({ name: "genre1" });
+      await genre.save();
+
+      const res = await request(server).get('/api/genres/' + genre._id);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('name', genre.name);
     });
   });
 });
