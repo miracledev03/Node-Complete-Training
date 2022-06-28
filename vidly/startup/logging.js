28dca2b4bc6c37334a63f9ac361adacda80c72d5
unknown <miracledev0303@gmail.com>
2022-06-28 13:02:18 -0300

    populating the test db

--- vidly/startup/logging.js
+++ vidly/startup/logging.js
@@ -3,10 +3,10 @@ const winston = require("winston");
 // require("winston-mongodb");
 
 module.exports = function () {
-  winston.handleExceptions(
-    new winston.transports.Console({ colorize: true, prettyPrint: true }),
-    new winston.transports.File({ filename: "uncaughtExceptions.log" })
-  );
+  // winston.handleExceptions(
+  //   new winston.transports.Console({ colorize: true, prettyPrint: true }),
+  //   new winston.transports.File({ filename: "uncaughtExceptions.log" })
+  // );
 
   process.on("unhandledRejection", (ex) => {
     throw ex;
