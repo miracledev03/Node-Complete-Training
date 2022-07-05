2bbe5a0b9b7b19fd87bbb8a61f006a9d940237b5
unknown <miracledev0303@gmail.com>
2022-07-05 18:55:43 -0300

    mongoose static methods

--- vidly/routes/returns.js
+++ vidly/routes/returns.js
@@ -8,10 +8,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", [auth, validate(validateReturn)], async (req, res) => {
-  Rental.findOne({
-    "customer._id": req.body.customerId,
-    "movie._id": req.body.movieId,
-  });
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
+
   if (!rental) return res.status(404).send("Rental not found.");
 
   if (rental.dateReturned)
