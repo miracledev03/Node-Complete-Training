d2eee73c91f1a4f391fe0e739b44d5500fd56fb3
unknown <miracledev0303@gmail.com>
2022-07-05 17:42:42 -0300

    looking up an object

--- vidly/routes/returns.js
+++ vidly/routes/returns.js
@@ -1,11 +1,19 @@
+const { Rental } = require("../models/rental");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  if (!req.body.customerId) return res.status(400).send('customerId not provided');
-  if (!req.body.movieId) return res.status(400).send('movieId not provided');
+  if (!req.body.customerId)
+    return res.status(400).send("customerId not provided");
+  if (!req.body.movieId) return res.status(400).send("movieId not provided");
 
-  res.status(401).send('Unauthorized');
+  Rental.findOne({
+    "customer._id": req.body.customerId,
+    "movie._id": req.body.movieId,
+  });
+  if (!rental) return res.status(404).send("Rental not found.");
+
+  res.status(401).send("Unauthorized");
 });
 
 module.exports = router;
